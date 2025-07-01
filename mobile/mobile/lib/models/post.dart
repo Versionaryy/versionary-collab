57d60385b09a2be295e260df7f0b7dc1e0329f13
Valentina Prado <valenpschool@gmail.com>
2025-07-01 09:54:02 -0300

    Remoção like

--- mobile/mobile/lib/models/post.dart
+++ mobile/mobile/lib/models/post.dart
@@ -5,16 +5,25 @@ import 'package:mobile/models/like.dart';
 
 part 'post.g.dart';
 
+
+enum CategoriaPost {
+  Bug,
+  Duvida,
+  Resolucao,
+}
 @JsonSerializable()
 class Post {
   int? id;
   String? titulo;
   String? descricao;
-  int? usuario_id;
+  int? usuarioId;
+  DateTime? atualizado_em;
+  DateTime? criado_em;
   Set<Comment>? comentarios = HashSet<Comment>();
   Set<Like>? curtidas = HashSet<Like>();
+  CategoriaPost? categoria;
 
-  Post(this.id, this.titulo, this.descricao, this.usuario_id, this.comentarios, this.curtidas);
+  Post(this.id, this.titulo, this.descricao, this.usuarioId, this.comentarios, this.curtidas);
   factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
   Map<String, dynamic> toJson() => _$PostToJson(this);
 }
--- mobile/mobile/lib/models/post.g.dart
+++ mobile/mobile/lib/models/post.g.dart
@@ -6,24 +6,46 @@ part of 'post.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Post _$PostFromJson(Map<String, dynamic> json) => Post(
-  (json['id'] as num?)?.toInt(),
-  json['titulo'] as String?,
-  json['descricao'] as String?,
-  (json['usuario_id'] as num?)?.toInt(),
-  (json['comentarios'] as List<dynamic>?)
-      ?.map((e) => Comment.fromJson(e as Map<String, dynamic>))
-      .toSet(),
-  (json['curtidas'] as List<dynamic>?)
-      ?.map((e) => Like.fromJson(e as Map<String, dynamic>))
-      .toSet(),
-);
+Post _$PostFromJson(Map<String, dynamic> json) =>
+    Post(
+        (json['id'] as num?)?.toInt(),
+        json['titulo'] as String?,
+        json['descricao'] as String?,
+        (json['usuarioId'] as num?)?.toInt(),
+        (json['comentarios'] as List<dynamic>?)
+            ?.map((e) => Comment.fromJson(e as Map<String, dynamic>))
+            .toSet(),
+        (json['curtidas'] as List<dynamic>?)
+            ?.map((e) => Like.fromJson(e as Map<String, dynamic>))
+            .toSet(),
+      )
+      ..atualizado_em =
+          json['atualizado_em'] == null
+              ? null
+              : DateTime.parse(json['atualizado_em'] as String)
+      ..criado_em =
+          json['criado_em'] == null
+              ? null
+              : DateTime.parse(json['criado_em'] as String)
+      ..categoria = $enumDecodeNullable(
+        _$CategoriaPostEnumMap,
+        json['categoria'],
+      );
 
 Map<String, dynamic> _$PostToJson(Post instance) => <String, dynamic>{
   'id': instance.id,
   'titulo': instance.titulo,
   'descricao': instance.descricao,
-  'usuario_id': instance.usuario_id,
+  'usuarioId': instance.usuarioId,
+  'atualizado_em': instance.atualizado_em?.toIso8601String(),
+  'criado_em': instance.criado_em?.toIso8601String(),
   'comentarios': instance.comentarios?.toList(),
   'curtidas': instance.curtidas?.toList(),
+  'categoria': _$CategoriaPostEnumMap[instance.categoria],
+};
+
+const _$CategoriaPostEnumMap = {
+  CategoriaPost.Bug: 'Bug',
+  CategoriaPost.Duvida: 'Duvida',
+  CategoriaPost.Resolucao: 'Resolucao',
 };
